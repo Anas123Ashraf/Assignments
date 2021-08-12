4d709e15208599ebae7ed4bf173ce6194a822220
Syed Muhammad Anas Ashraf <86324397+Anas123Ashraf@users.noreply.github.com>
2021-08-12 21:16:14 +0500

    Add files via upload

---
+++ table_task.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<title>ANAS</title>
+	</head>
+	
+	<body style="background:dodgerblue;">
+		<h2 style="text-align:center;"><u>MY SCHOOL TIME TABLE</u></h2>
+	 	<center> 
+			<table style="text-align: center;"border="1"width="50px">
+				<tr>
+					<th>Duration</th>
+					<th>Monday</th>
+					<th>Tuesday</th>
+					<th>Wednesday</th>
+					<th>Thursday</th>
+					<th>Friday</th>
+				</tr>
+				
+				<tr>
+					<td>8:00-8:40</td>
+					<td>math</td>
+					<td>geograp</td>
+					<td>physics</td>
+					<td>computer</td>
+					<td>analysis</td>
+				<tr>
+					<td>8:40-9:20</td>
+					<td>English</td>
+					<td>Science</td>
+					<td>Biology</td>
+					<td>Chemistry</td>
+					<td>Urdu</td>
+				</tr>
+				
+				<tr>
+					<td>9:20-10:00</td>
+					<td>Library</td>
+					<td>phy-lab</td>
+					<td>che-lab</td>
+					<td>com-lab</td>
+					<td>Statics</td>
+				</tr>
+				
+				<tr style="background:yellow;">
+					<td>10:00-10:20</td>
+					<td>Break</td>
+					<td>Break</td>
+					<td>Break</td>
+					<td>Break</td>
+					<td>Break</td>
+				</tr>
+
+				<tr>
+					<td>10:20-11:00</td>
+					<td>math-2</td>
+					<td>computer</td>
+					<td>physics</td>
+					<td>chemistry</td>
+					<td>bio-lab</td>
+				</tr>
+
+				<tr>
+					<td>11:00-11:40</td>
+					<td>MECHANICS</td>
+					<td>THERMODYNAMICS</td>
+					<td>Machine-Design</td>
+					<td>Workshop</td>
+					<td>CNC</td>
+				</tr>
+
+				<tr style="background:red;">
+					<td>11:00-12:00</td>
+					<td>Luch</td>
+					<td>Luch</td>
+					<td>Luch</td>
+					<td>Luch</td>
+					<td>Luch</td>
+				</tr>
+
+				<tr>
+					<td>12:00-12:40</td>
+					<td>Islamiyat</td>
+					<td>Pak-Study</td>
+					<td>Grammer</td>
+					<td>Spoken</td>
+					<td>Painting</td>
+				</tr>
+			</table>
+		</center>
+		script
+	<scriptbasic.js>
+		
+
+	</body>
+	
+</html>
+
+bs
